daa31b10103f6720f607eaa6d8a777abadd90d36
Lanka Rathnayake <LankaR@99x.io>
2022-03-22 09:48:51 +0530

    adding the logging route

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,15 +1,42 @@
 const router = require('express').Router();
 
 const UsersController = require('../controllers/users');
-const User = require('../models/user.model');
+const UserModel = require('../models/user.model');
+const { hashSync, compareSync } = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-router.get('/profile', async(req, res, next) => {
-    console.log("req.user", req.user)
-    res.send("LogProfilein")
-})
 
-router.get('/userdata', async(req, res, next) => {
-    res.send("Userdata")
+router.post('/login', (req, res) => {
+    UserModel.findOne({ email: req.body.email }).then(user => {
+        if (!user) {
+            return res.status(401).send({
+                success: false,
+                message: "Could not find the user."
+            })
+        }
+
+        //Incorrect password
+        if (!compareSync(req.body.password, user.password)) {
+            return res.status(401).send({
+                success: false,
+                message: "Incorrect password"
+            })
+        }
+
+        const payload = {
+            email: user.email,
+            id: user._id
+        }
+
+        const token = jwt.sign(payload, "Random string", { expiresIn: "1d" })
+
+        return res.status(200).send({
+            success: true,
+            message: "Logged in successfully!",
+            token: "Bearer " + token
+        })
+
+    })
 })
 
 router.post('/register', async(req, res, next) => {
@@ -17,9 +44,10 @@ router.post('/register', async(req, res, next) => {
         const { email } = req.body;
         const doesExist = await User.findOne({ email });
         if (doesExist) {
-            res.send({ message: "user already exist" })
-                // res.redirect('/auth/register');
-            return;
+            return res.status(200).json({
+                success: true,
+                redirectUrl: '/'
+            })
         }
         const user = new User(req.body);
         await user.save().then(user => {
@@ -44,12 +72,4 @@ router.post('/register', async(req, res, next) => {
         res.status(500).send(error)
     }
 })
-
-
-router.post('/login', async(req, res, next) => {
-    res.send("Login")
-})
-
-
-
 module.exports = router

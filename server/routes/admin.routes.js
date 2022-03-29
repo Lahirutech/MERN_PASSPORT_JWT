c7185853aa5abfbd74e40680adf4b91f3eb7d43d
Lanka Rathnayake <LankaR@99x.io>
2022-03-30 00:05:49 +0530

    adding the admin route

---
+++ server/routes/admin.routes.js
@@ -0,0 +1,13 @@
+const { jwtUserAuth } = require("../utils/Auth.utils");
+
+
+const router = require("express-promise-router")()
+
+router.get(
+    "/profile", jwtUserAuth,
+    async(req, res) => {
+        return res.json("Hello admin profile");
+    }
+);
+
+module.exports = router

640b0842846dfa9cca34c54741301f561eb467df
Lanka Rathnayake <LankaR@99x.io>
2022-04-03 16:03:49 +0530

    add the kogin.js

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,5 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Login() {
-  return <div>Login</div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submit = () => {
+    console.log(username, password);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Enter Username"
+        value={username}
+        onChange={(event) => setUsername(event.target)}
+      />
+
+      <input
+        type="password"
+        placeholder="Enter password"
+        value={password}
+        onChange={(event) => setUsername(event.target)}
+      />
+      <button onClick={submit}></button>
+    </div>
+  );
 }

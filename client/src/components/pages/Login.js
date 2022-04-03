75bb59b2a60576c4b496b78b3f345ef2a4e8552a
Lanka Rathnayake <LankaR@99x.io>
2022-04-03 16:58:24 +0530

    adding the post from front end

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function Login() {
   const [username, setUsername] = useState("");
@@ -6,6 +7,18 @@ export default function Login() {
 
   const submit = () => {
     console.log(username, password);
+    const userData = {
+      email: username,
+      password: password,
+    };
+    axios
+      .post("http://localhost:5001/user/login", { username, password })
+      .then((user) => {
+        console.log(user);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   return (
@@ -14,16 +27,16 @@ export default function Login() {
         type="text"
         placeholder="Enter Username"
         value={username}
-        onChange={(event) => setUsername(event.target)}
+        onChange={(event) => setUsername(event.target.value)}
       />
 
       <input
         type="password"
         placeholder="Enter password"
         value={password}
-        onChange={(event) => setUsername(event.target)}
+        onChange={(event) => setPassword(event.target.value)}
       />
-      <button onClick={submit}></button>
+      <button onClick={submit}>Login</button>
     </div>
   );
 }

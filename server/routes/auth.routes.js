3cad5c34d5a17ac7c091967482e581ea8d1d68cc
Lanka Rathnayake <LankaR@99x.io>
2022-04-13 01:56:51 +0530

    adding the auth route

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,15 +1,28 @@
 const passport = require("passport");
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.get('/protected', passport.authenticate('jwt', { session: false }), (req, res) => {
+router.get(
+  "/protected",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
     return res.status(200).send({
-        success: true,
-        user: {
-            id: req.user._id,
-            email: req.user.email,
-            role: req.user.role
-        }
-    })
-})
+      success: true,
+      user: {
+        id: req.user._id,
+        email: req.user.email,
+        role: req.user.role,
+      },
+    });
+  }
+);
 
-module.exports = router
+router.get(
+  "/authenticated",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    console.log("req", req.user);
+    const { email, role } = req.user;
+    res.status(200).json({ isAuthenticated: true, user: { email, role } });
+  }
+);
+module.exports = router;

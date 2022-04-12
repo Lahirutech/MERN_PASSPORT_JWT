03986d7b5190a71df03e5a65374ae9b226e8cefc
Lanka Rathnayake <LankaR@99x.io>
2022-04-13 01:44:30 +0530

    Adding the private route

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,26 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/pages/Login";
 import Protected from "./components/pages/Protected";
 
+import PrivateRoute  from "./hocs/PrivateRoute";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          {/* <Route path="/" element={<App />} /> */}
-          <Route path="login" element={<Login />} />
-          <Route path="protected" element={<Protected />} />
+          <Route path="/login" element={<Login />} />
+          {/* <Route
+            path="/protected"
+            
+            element={<PrivateRoute roles={["CLIENT", "ADMIN"]}> <Protected /> </PrivateRoute>}
+          /> */}
+          <Route
+          path="/protected"
+          element={
+            <PrivateRoute roles={["CLIENT", "ADMIN"]}>
+              <Protected />
+            </PrivateRoute>
+          }
+        />
         </Routes>
       </BrowserRouter>
       ,

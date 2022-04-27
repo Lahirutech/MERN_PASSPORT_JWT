87dc5a4fbd7cf7648707d2a4fb4be7d4027a180c
Lanka Rathnayake <LankaR@99x.io>
2022-04-28 03:15:30 +0530

    modifying the protected page

--- client/src/components/pages/Protected.js
+++ client/src/components/pages/Protected.js
@@ -4,22 +4,22 @@ import { useNavigate } from "react-router-dom";
 
 export default function Protected() {
   let navigate = useNavigate();
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    console.log("protected loaded");
-    axios
-      .get("http://localhost:5001/auth/protected", {
-        headers: {
-          Authorization: token,
-        },
-      })
-      .then((res) => {
-        console.log("response",res);
-      })
-      .catch((err) => {
-        console.log(err);
-        navigate("/login");
-      });
-  }, []);
+  // useEffect(() => {
+  //   const token = localStorage.getItem("token");
+  //   console.log("protected loaded");
+  //   axios
+  //     .get("http://localhost:5001/auth/protected", {
+  //       headers: {
+  //         Authorization: token,
+  //       },
+  //     })
+  //     .then((res) => {
+  //       console.log("response",res);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //       navigate("/login");
+  //     });
+  // }, []);
   return <div> Protected  component</div>;
 }

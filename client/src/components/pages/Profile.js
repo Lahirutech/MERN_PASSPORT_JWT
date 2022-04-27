4f503fc2470ed429caf74abae212b8c4ac2ac716
Lanka Rathnayake <LankaR@99x.io>
2022-04-28 03:14:45 +0530

    Adding the contexthook in react

--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { AuthContext } from "../../Context/AuthContext";
 
 export default function Profile() {
+    const { isAuthenticated, user } = useContext(AuthContext);
+console.log("isAuthenticated from Profile",isAuthenticated)
   return (
     <div>Profile Page protected</div>
   )
